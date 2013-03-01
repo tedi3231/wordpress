e3f4fcb36d230071acc6ad2e458eb4f16ed8fd20
tedi32321 <tedi3231@gmail.com>
2013-03-01 14:56:05 +0800

    in

--- wp-content/themes/wedo/infrastructuretemplate.php
+++ wp-content/themes/wedo/infrastructuretemplate.php
@@ -6,6 +6,7 @@
 ?>
 <?php get_header();?>
 
+<!-- 
 <div class="top-single">
     <img src="<?php bloginfo("template_url"); ?>/images/banner1.png"></div>
 <div class="main">
@@ -23,7 +24,66 @@
             </div>
         </div>
     </div>
-    <div class="sidebar  sidebar-right">
+-->
+
+        <!-- START TOP -->
+        <div class="top-single">
+            <img src="<?php bloginfo("template_url"); ?>/images/banner1.png"></div>
+        </div>
+        <!-- END TOP -->
+
+        <!-- START MAIN -->
+        <div class="main">
+            <div class="container_12">
+                <div class="main-continer continer-left">
+                    <div class="grid_8">
+                        <div class="sidebar-content">
+                            <h3>Network</h3>
+<p>A network infrastructure is an interconnected group of computer systems linked by the various parts of a telecommunications architecture. Specifically, this infrastructure refers to the organization of its various parts and their configuration — from individual networked computers to routers, cables, wireless access points, switches, backbones, network protocols, and network access methodologies. Infrastructures can be either open or closed, such as the open architecture of the Internet or the closed architecture of a private intranet. They can operate over wired or wireless network connections, or a combination of both. </p>
+                            <h5>Overview</h5>
+                            <p>Developed from the start as a network operating system, Microsoft Windows 2000 Server continues to improve its presence on networks and the Internet. Microsoft is following the worldwide trend of using the Internet for as much as possible.</p>
+                            <p>Windows 2000 Server will help companies make better use of their Internet connections. By providing support for additional standardized features of TCP/IP, Microsoft has improved the performance of its premier network operating system both for communications with other Windows systems and with UNIX systems. Technologies such as virtual private networks (VPNs) will allow organizations to reduce costs without sacrificing security. The routing features built in to Windows 2000 servers allow those servers to act as routers, with graphical user interfaces far superior to those of hardware-based routers. The new Quality of Service (QoS) standards allow more consistent and reliable networking, especially when using real-time audio and video.</p>
+<hr class="hr-sidebar"/>
+                            <div class="frame_left img-border zoom">
+                                <a class="preload" href="images/content/blog/1-all.jpg" rel="prettyPhoto" title="This is the description">
+                                    <img src="images/content/featured-products/1.jpg" alt=""/>
+                                </a>
+                                <span class="image-desc">It is image caption</span>
+                            </div>
+                            <h5>Title 11111111111111111</h5>
+                            <p>A network infrastructure is an interconnected group of computer systems linked by the various parts of a telecommunications architecture. Specifically, this infrastructure refers to the organization of its various parts and their configuration — from individual networked computers to routers, cables, wireless access points, switches, backbones, network protocols, and network access methodologies. Infrastructures can be either open or closed, such as the open architecture of the Internet or the closed architecture of a private intranet. They can operate over wired or wireless network connections, or a combination of both. A network infrastructure is an interconnected group of computer systems linked by the various parts of a telecommunications architecture. Specifically, this infrastructure refers to the organization of its various parts and their configuration — from individual networked computers to routers, cables, wireless access points, switches, backbones, network protocols, and network access methodologies. Infrastructures can be either open or closed, such as the open architecture of the Internet or the closed architecture of a private intranet. They can operate over wired or wireless network connections, or a combination of both. </p>
+                        <hr class="hr-sidebar"/>
+                            <div class="relative frame_right zoom">
+                                <div class="img-border article-image-slide image-default-size">
+                                    <div>
+                                        <a class="preload imgMedium item-preview iconZoom" href="images/content/blog/2-all.jpg" rel="prettyPhoto[gallery2]" title="This is the description">
+                                            <img src="images/content/portfolio/4/2.jpg" alt="" />
+                                        </a>
+                                    </div>
+                                    <div>
+                                        <a class="preload imgMedium item-preview iconPlay" href="images/content/blog/1-all.jpg" rel="prettyPhoto[gallery2]" title="This is the description">
+                                            <img src="images/content/portfolio/4/1.jpg" alt="" />
+                                        </a>
+                                    </div>
+                                    <div>
+                                        <a class="preload imgMedium item-preview iconZoom" href="images/content/blog/3-all.jpg" rel="prettyPhoto[gallery2]" title="This is the description">
+                                            <img src="images/content/portfolio/4/3.jpg" alt="" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <h5>Title 2222222222222222</h5>
+                            <p>A network infrastructure is an interconnected group of computer systems linked by the various parts of a telecommunications architecture. Specifically, this infrastructure refers to the organization of its various parts and their configuration — from individual networked computers to routers, cables, wireless access points, switches, backbones, network protocols, and network access methodologies. Infrastructures can be either open or closed, such as the open architecture of the Internet or the closed architecture of a private intranet. They can operate over wired or wireless network connections, or a combination of both. </p>
+                          <hr class="hr-sidebar"/>
+                            <h5>Summary</h5>
+                            <p>A network infrastructure is an interconnected group of computer systems linked by the various parts of a telecommunications architecture. Specifically, this infrastructure refers to the organization of its various parts and their configuration — from individual networked computers to routers, cables, wireless access points, switches, backbones, network protocols, and network access methodologies. Infrastructures can be either open or closed, such as the open architecture of the Internet or the closed architecture of a private intranet. They can operate over wired or wireless network connections, or a combination of both. 
+                            </p>
+
+                            <hr class="hr-sidebar"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="sidebar  sidebar-right">
                     <div class="grid_3">
                         <div class="sidebar-top">
                             <div class="sidebar-widget">
@@ -76,5 +136,7 @@
                     </div>
                 </div>
                 <div class="clear"></div>
-</div>
-<?php get_footer();?>
+            </div>
+        </div>
+        <!-- END MAIN -->
+        <?php get_footer();?>
