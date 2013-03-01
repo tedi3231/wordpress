9a8c435b52306e3f76c7b38a0e9b5a97ac14f497
tedi32321 <tedi3231@gmail.com>
2013-03-01 13:52:13 +0800

    set wedo site config file

--- wp-config.php
+++ wp-config.php
@@ -15,16 +15,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress 数据库的名称 */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'wedoweb');
 
 /** MySQL 数据库用户名 */
 define('DB_USER', 'root');
 
 /** MySQL 数据库密码 */
-define('DB_PASSWORD', 'loveyuanyuan');
+define('DB_PASSWORD', 'p@ssw0rd');
 
 /** MySQL 主机 */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
